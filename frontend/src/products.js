9a45d6de1554a772a24f7ac970e98c136b2d2385
mominur-emon <mominur.emon19@gmail.com>
2023-10-19 23:08:06 +0600

    1st commit frontend

---
+++ frontend/src/products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    _id: "1",
+    name: "Airpods Wireless lrem lorem proke some hits ",
+    image: "/images/airpods.jpg",
+    description:
+      "Dolor est kasd et labore tempor consetetur, dolor no gubergren sit vero sit. Vero amet sea sed lorem dolores nonumy. ",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 10,
+    rating: 0.5,
+    numReviews: 2,
+  },
+  {
+    _id: "2",
+    name: "Camera lorem text",
+    image: "/images/camera.jpg",
+    description:
+      "Dolor est kasd et labore tempor consetetur, dolor no gubergren sit vero sit. Vero amet sea sed lorem dolores nonumy. ",
+    brand: "Samsung",
+    category: "Electronics",
+    price: 79.91,
+    countInStock: 11,
+    rating: 2.5,
+    numReviews: 9,
+  },
+  {
+    _id: "3",
+    name: "mouse lorem text",
+    image: "/images/mouse.jpg",
+    description:
+      "Dolor est kasd et labore tempor consetetur, dolor no gubergren sit vero sit. Vero amet sea sed lorem dolores nonumy. ",
+    brand: "HP",
+    category: "Electronics",
+    price: 79.91,
+    countInStock: 8,
+    rating: 3.0,
+    numReviews: 14,
+  },
+  {
+    _id: "4",
+    name: "sample lorem text",
+    image: "/images/camera.jpg",
+    description:
+      "Dolor est kasd et labore tempor consetetur, dolor no gubergren sit vero sit. Vero amet sea sed lorem dolores nonumy. ",
+    brand: "Nokia",
+    category: "Electronics",
+    price: 79.91,
+    countInStock: 11,
+    rating: 4.0,
+    numReviews: 9,
+  },
+];
+
+export default products;
